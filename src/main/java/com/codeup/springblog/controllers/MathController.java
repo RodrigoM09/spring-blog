4ceb6eb9054f9935610af64e80f6989a1e30925d
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-11-15 15:36:32 -0600

    spring-blog

--- src/main/java/com/codeup/springblog/controllers/MathController.java
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -2,29 +2,30 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
 
-    @GetMapping("/add/3/and/4")
+    @GetMapping("/add/{num1}/and/{num2}")
     @ResponseBody
-    public String addController(){
-        return "7";
+    public int addController(@PathVariable int num1, @PathVariable int num2){
+        return num1 + num2;
     }
-    @GetMapping("/subtract/3/from/10")
+    @GetMapping("/subtract/{num1}/from/{num2}")
     @ResponseBody
-    public String subtractController(){
-        return "7";
+    public int subtractController(@PathVariable int num1, @PathVariable int num2){
+        return num1 - num2;
     }
-    @GetMapping("/multiply/4/and/5")
+    @GetMapping("/multiply/{num1}/and/{num2}")
     @ResponseBody
-    public String multiplyController(){
-        return "20";
+    public int multiplyController(@PathVariable int num1, @PathVariable int num2){
+        return num1 * num2;
     }
-    @GetMapping("/divide/6/by/3")
+    @GetMapping("/divide/{num1}/by/{num2}")
     @ResponseBody
-    public String divideController(){
-        return "2";
+    public int divideController(@PathVariable int num1, @PathVariable int num2){
+        return num1 / num2;
     }
 }
