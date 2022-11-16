1442be84c080874220933df73476cc15ea85699b
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-11-16 14:56:23 -0600

    coffee

--- src/main/java/com/codeup/springblog/controllers/CoffeeController.java
+++ src/main/java/com/codeup/springblog/controllers/CoffeeController.java
@@ -1,10 +1,12 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Coffee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/coffee")
@@ -17,7 +19,17 @@ public class CoffeeController {
 
     @GetMapping("/{roast}")
     public String roast(@PathVariable String roast, Model model){
-        model.addAttribute("roast", roast);
+        Coffee selection = new Coffee(roast, "Cool Beans");
+        Coffee selection2 = new Coffee(roast, "Coffee Bros");
+        selection.setOrigin("Ethiopia");
+        selection2.setOrigin("Vietnam");
+        List<Coffee> selections = new ArrayList<>(List.of(selection, selection2));
+        model.addAttribute("selections", selections);
+        return "Coffee";
+    }
+    @PostMapping
+    public String signup(@RequestParam(name="email")String email, Model model){
+        model.addAttribute("email", email);
         return "Coffee";
     }
 }
