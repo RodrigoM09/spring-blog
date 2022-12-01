d081b4008495b8ed617472d6a765c95ae99b5ef4
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-12-01 09:03:11 -0600

    spring blog

--- src/main/java/com/codeup/springblog/controllers/UserController.java
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Users;
 import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     private final UserRepository usersDao;
 
-    public UserController(UserRepository usersDao){
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository usersDao, PasswordEncoder passwordEncoder){
         this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register")
@@ -24,6 +28,7 @@ public class UserController {
 
     @PostMapping("/register")
     public String registerUser(@ModelAttribute Users user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         usersDao.save(user);
         return "redirect:/posts/create";
     }
